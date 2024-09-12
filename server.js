ca319641a680455f05535fb7302d8aca8ae9cd85
omar lengliz <omarlengliz12@gmail.com>
2024-09-12 14:20:25 +0100

    remove trans ffmpeg

--- server.js
+++ server.js
@@ -12,29 +12,7 @@ const config = {
     webroot: './www',
     allow_origin: '*',
   },
-  trans: {
-    
-    ffmpeg: 'F:/programfiles/ffmpg/bin/ffmpeg.exe', // Corrected path format
-    tasks: [
-      {
-        app: 'live',
-        mp4: true,
-        mp4Flags: '[movflags=frag_keyframe+empty_moov]',
-        hls: true,                // Enable HLS for adaptive streaming
-        hlsFlags: '[hls_time=1:hls_list_size=3:hls_flags=delete_segments]',  // Adjust HLS settings for lower latency
-        dash: true,
-        
-        args: [
-          '-preset', 'ultrafast',  // Use ultrafast preset for lower latency
-          '-tune', 'zerolatency',  // Tune for real-time, low-latency streaming
-          '-fflags', 'nobuffer',
-          '-flags', 'low_delay',
-          '-movflags', 'faststart',
-          '-g', '30'               // GOP size (smaller GOP can reduce latency)
-        ],
-      },
-    ],
-  },
+  
 };
 
 const nms = new NodeMediaServer(config);
